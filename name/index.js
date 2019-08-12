1c71cd514e21503d6e3c6b19bc525ffa328e5ef7
federico <peteco1983@outlook.com>
2019-08-12 15:56:00 +0200

    Corrected file

--- name/index.js
+++ name/index.js
@@ -2,9 +2,9 @@ var firstname = "Federico";
 var lastname = "Ientile";
 console.log(firstname, lastname); 
 
-var name="Robert Schulz"
-var city="Frankfurt am Main"
-var job="Butcher"
-var hobby="Play Soccer"
-var favdish="Pizza"
-console.log=(name,city,job,hobby,favdish)
+var name="Robert Schulz";
+var city="Frankfurt am Main";
+var job="Butcher";
+var hobby="Play Soccer";
+var favdish="Pizza";
+console.log=(name, city, job, hobby, favdish);
