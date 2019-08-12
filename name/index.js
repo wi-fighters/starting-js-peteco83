5b77a4299d7defbdbaf21894971c95e60da5e62e
federico <peteco1983@outlook.com>
2019-08-12 15:53:59 +0200

    Add info from a person

--- name/index.js
+++ name/index.js
@@ -1,3 +1,10 @@
 var firstname = "Federico";
 var lastname = "Ientile";
 console.log(firstname, lastname); 
+
+var name="Robert Schulz"
+var city="Frankfurt am Main"
+var job="Butcher"
+var hobby="Play Soccer"
+var favdish="Pizza"
+console.log=(name,city,job,hobby,favdish)
