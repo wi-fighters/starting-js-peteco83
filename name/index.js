c7a1e0128c32f272cbc0d49e46bf037f3e32dcac
federico <peteco1983@outlook.com>
2019-08-13 10:14:34 +0200

    Exercise 8

--- name/index.js
+++ name/index.js
@@ -1,6 +1,9 @@
+// 1
 var firstName = "Federico";
 var lastName = "Ientile";
 console.log(firstName, lastName); 
+
+//2
 
 var name="Robert Schulz";
 var city="Frankfurt am Main";
@@ -9,8 +12,12 @@ var hobby="Soccer";
 var favdish="pizza";
 console.log(name, city, job, hobby, favdish);
 
+//3
+
 console.log(name+" "+"is a"+" "+job+" "+"who lives in"+" "+city+" "+"and who plays"+" "+hobby+"."+" "+"He loves"+" "
 +favdish+" "+"and goes every Saturday to a Pizzeria.")
+
+//4
 
 let length = 16.78; //is a number 
 let lastlastName = "Johnson"; //is a string
@@ -21,8 +28,28 @@ console.log(typeof lastlastName)
 console.log(typeof car)
 console.log(typeof isOpen)
 
+//5
+
 let ageMark = "19"
 let ageJohn = 34
 console.log(typeof ageMark)
 console.log(typeof ageJohn)
+
+//6
+
 console.log((ageMark>ageJohn) ? "Yes, Mark is older than John" : "No, John is older than Mark")
+
+//7
+
+let b = 7
+let a = "9"
+let c = 7
+console.log(b===a)
+console.log(c!==b)
+console.log(b!==a)
+
+//8
+
+let programmingLanguage = "Javascript"
+let isFun = true
+console.log(programmingLanguage+" is Fun:"+isFun)
