131dcb36e12472adbe8ab3cf93b124829fd0fc03
federico <peteco1983@outlook.com>
2019-08-13 09:58:05 +0200

    Exercise 6

--- name/index.js
+++ name/index.js
@@ -1,14 +1,28 @@
-/*
+var firstName = "Federico";
+var lastName = "Ientile";
+console.log(firstName, lastName); 
+
+var name="Robert Schulz";
+var city="Frankfurt am Main";
+var job="butcher";
+var hobby="Soccer";
+var favdish="pizza";
+console.log(name, city, job, hobby, favdish);
+
+console.log(name+" "+"is a"+" "+job+" "+"who lives in"+" "+city+" "+"and who plays"+" "+hobby+"."+" "+"He loves"+" "
++favdish+" "+"and goes every Saturday to a Pizzeria.")
+
 let length = 16.78; //is a number 
-let lastName = "Johnson"; //is a string
+let lastlastName = "Johnson"; //is a string
 let car = ""; // is a string
 let isOpen = true; //is a boolean value
 console.log(typeof length);
-console.log(typeof lastName)
+console.log(typeof lastlastName)
 console.log(typeof car)
-console.log(typeof isOpen)*/
+console.log(typeof isOpen)
 
 let ageMark = "19"
 let ageJohn = 34
 console.log(typeof ageMark)
 console.log(typeof ageJohn)
+console.log((ageMark>ageJohn) ? "Yes, Mark is older than John" : "No, John is older than Mark")
