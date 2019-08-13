9ac3172fc673160f23c0471be85bdb022cfde9dc
federico <peteco1983@outlook.com>
2019-08-13 14:23:14 +0200

    To add Exercise 1 and 2 of Arithmetics

---
+++ arithmetic/index.js
@@ -0,0 +1,34 @@
+//1
+
+let x = 17;
+let z = 30;
+console.log((x/z)*100);
+
+//2 
+
+const a = 8;
+const b = 6;
+const c = 5;
+const d = 9;
+const e = 8;
+const f = 2;
+const g = 1;
+const h = 8.5;
+const i = 7;
+const j = 4;
+let averageDays = (a+b+c+d+e+f+g+h+i+j);
+console.log((averageDays)/10);
+
+// 3
+
+let first = 75;
+let second = 70;
+let third = 85;
+let fourth = 90;
+let fifth = 100;
+let averageTest = 85;
+let firstPart = 85 * 6;
+let secondPart = first+second+third+fourth+fifth;
+console.log(`Score in the sixth test is `+(firstPart-secondPart))
+
+
