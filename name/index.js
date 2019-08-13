dd080d826f6fe2e17ca2eb1df23b9a6d0aa6a48c
federico <peteco1983@outlook.com>
2019-08-13 09:37:24 +0200

    Exercise 5

--- name/index.js
+++ name/index.js
@@ -1,13 +1,8 @@
-var firstName = "Federico";
-var lastName = "Ientile";
-console.log(firstName, lastName); 
-
-var name="Robert Schulz";
-var city="Frankfurt am Main";
-var job="butcher";
-var hobby="Soccer";
-var favdish="pizza";
-console.log(name, city, job, hobby, favdish);
-
-console.log(name+" "+"is a"+" "+job+" "+"who lives in"+" "+city+" "+"and who plays"+" "+hobby+"."+" "+"He loves"+" "
-+favdish+" "+"and goes every Saturday to a Pizzeria.")
+let length = 16.78; //is a number 
+let lastName = "Johnson"; //is a string
+let car = ""; // is a string
+let isOpen = true; //is a boolean value
+console.log(typeof length);
+console.log(typeof lastName)
+console.log(typeof car)
+console.log(typeof isOpen)
