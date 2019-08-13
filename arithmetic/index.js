a4a3810731ee2bab6c552b6f250435a282dd7a10
federico <peteco1983@outlook.com>
2019-08-13 15:41:06 +0200

    Add up to exercise 7 arith

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -2,37 +2,63 @@
 
 let x = 17;
 let z = 30;
-console.log((x/z)*100);
+console.log(`${x/z*100}%`)
 
 //2 
 
-const a = 8;
-const b = 6;
-const c = 5;
-const d = 9;
-const e = 8;
-const f = 2;
-const g = 1;
-const h = 8.5;
-const i = 7;
-const j = 4;
-let averageDays = (a+b+c+d+e+f+g+h+i+j);
-console.log((averageDays)/10);
+const a = 8+6+5+9+8+2+1+8.5+7+4;
+console.log(a/10);
 
-let first = 75;
-let second = 70;
-let third = 85;
-let fourth = 90;
-let fifth = 100;
-let averageTest = 85;
+//3
+
 let firstPart = 85 * 6;
-let secondPart = first+second+third+fourth+fifth;
+let secondPart = 75+70+85+90+100;
 console.log(`Score in the sixth test is ${firstPart-secondPart}`)
 
-//3 
+//4 
 
 let primero = 80
 let segundo = 78
 let primer1 = 80 * 9
 let segun2 = 78 * 8
 console.log(`James needs a minimum of ${(primer1-segun2)}% to get an 80% average`)
+
+//5
+
+const tip1 = 22.35 
+const tip2 = 26.67
+const tip3 = 35.92
+let perc1 = 10/100
+let perc2 = 15/100
+let perc3 = 20/100
+console.log(tip1+perc1)
+console.log(tip2+perc2)
+console.log(tip3+perc3)
+
+//6
+
+let side = 4.74
+console.log(`The perimeter of the square is ${side*4}cm`)
+
+let sidea = 5
+let sideb = 6
+let sidec = 7
+console.log(`The perimeter of the triangle is ${sidea+sideb+sidec}cm`)
+
+let squareSide = 5
+let squareArea = squareSide * squareSide
+console.log(`The area of the square is ${squareArea}cm`)
+
+let cubeSide = 9
+let cubeVolume = cubeSide * cubeSide * cubeSide
+console.log(`The volume of the cube is ${cubeVolume}cm`)
+
+// 7 
+
+let totalTickets = 6450
+let ticket = 15
+console.log(`${totalTickets/ticket} persons purchased a ticket`)
+
+let income = 500
+let weeksYear = 52
+console.log(`Sylvia makes ${income*weeksYear} USD every year`)
