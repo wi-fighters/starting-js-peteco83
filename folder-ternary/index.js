218452e4ae539472d29f2e4bf6f7ea5629afcd6c
federico <peteco1983@outlook.com>
2019-08-14 10:22:58 +0200

    To add Ternary exercise

--- folder-ternary/index.js
+++ folder-ternary/index.js
@@ -19,20 +19,11 @@ const allowedAge = 21;
 const alcoholAge = (personAge <= allowedAge)? "Here's some juice" : "Here's some wine";
 console.log(alcoholAge)
 
-/*### 4. Student Discount
-Declare a variable named isStudent. 
-If true, print `Ticket costs €5,00`. If false, print `Ticket costs €12,00`.*/
-
 // 4.
 
 const isStudent = "student";
-
-const student = ("student = student") ? "Ticket costs €5,00" : "Ticket costs €12,00";
+const student = (isStudent === "student") ? "Ticket costs €5,00" : "Ticket costs €12,00";
 console.log(student)
-
-/*### 5. What time is it? 
-Declare a variable named timeOfDay. Check if the time of day is `morning` - 
-if so, print `Good Morning`. If not, print `Good Evening`.*/
 
 // 5.
 
