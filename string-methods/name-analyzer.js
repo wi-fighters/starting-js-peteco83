80378b467cd753d92e1a7d133042fb86c4870a3a
federico <peteco1983@outlook.com>
2019-08-15 09:33:44 +0200

    Add name analyzer exercise

---
+++ string-methods/name-analyzer.js
@@ -0,0 +1,17 @@
+/*let name = "Winston"
+let totalLetters = name.length
+let firstLetter = name[0]
+let lastLetter = name[name.length -1]
+console.log(`Hello ${name}.
+Your name is ${totalLetters} letters long
+The first letter of your name is ${firstLetter}, the last is ${lastLetter}.
+If I wanted to be funny, I would call you Winschmoo`)*/
+
+let name = "Winston Churchill"
+let totalLetters = name.length
+let firstLetter = name[0]
+let lastLetter = name[name.length -1]
+console.log(`Hello ${name}.
+Your name is ${totalLetters} letters long
+The first letter of your name is ${firstLetter}, the last is ${lastLetter}.
+If I wanted to be funny, I would call you Winschmoo`)
