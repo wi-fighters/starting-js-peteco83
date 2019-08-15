c76906ff3101ce771d1d1587b1a52ff74b3c2d8c
federico <peteco1983@outlook.com>
2019-08-15 09:51:54 +0200

    Add name analyzer exercise

--- string-methods/name-analyzer.js
+++ string-methods/name-analyzer.js
@@ -7,7 +7,7 @@ Your name is ${totalLetters} letters long
 The first letter of your name is ${firstLetter}, the last is ${lastLetter}.
 If I wanted to be funny, I would call you Winschmoo`)*/
 
-let name = "Winston Churchill"
+let name = "Winston Churchill" //no deberia contar el espacio
 let totalLetters = name.length
 let firstLetter = name[0]
 let lastLetter = name[name.length -1]
