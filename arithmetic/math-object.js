b59b3314d10c20365de2971557e73691b3c4a17d
federico <peteco1983@outlook.com>
2019-08-19 15:52:40 +0200

    To correct some values

--- arithmetic/math-object.js
+++ arithmetic/math-object.js
@@ -2,24 +2,14 @@ const lowNumber = Math.min(1, 2, 3, 6, 5648967, 0.5, 7878789978)
 console.log(`The number with the lowest value is ${lowNumber}`) 
 
 const highNumber = Math.max(6544,65456987,98756321,65489,11,1221)
-console.log(`The number with the lowest value is ${highNumber}`)
+console.log(`The number with the highest value is ${highNumber}`)
 
-// const roundUp1 = 3321.32321
-// const roundUp2 = 326.76
-// const roundUp3 = 76788.7
-// const roundUp4 = -9.78
-// const roundUp5 = 43.342
 console.log(Math.ceil(3321.32321))
 console.log(Math.ceil(326.76))
 console.log(Math.ceil(76788.7))
 console.log(Math.ceil(-9.78))
 console.log(Math.ceil(43.342))
 
-// const roundDown1 = 3321.32321
-// const roundDown2 = 326.76
-// const roundDown3 = 76788.7
-// const roundDown4 = -9.78
-// const roundDown5 = 28.329
 console.log(Math.floor(3321.32321))
 console.log(Math.floor(326.76))
 console.log(Math.floor(76788.7))
--- string-methods/name-analyzer.js
+++ string-methods/name-analyzer.js
@@ -7,11 +7,12 @@ Your name is ${totalLetters} letters long
 The first letter of your name is ${firstLetter}, the last is ${lastLetter}.
 If I wanted to be funny, I would call you Winschmoo`)*/
 
-let name = "Winston Churchill" //no deberia contar el espacio
-let totalLetters = name.length
-let firstLetter = name[0]
-let lastLetter = name[name.length -1]
+const name = "Winston Churchill" //no deberia contar el espacio
+const totalLetters = name.length
+const spaceCount = (name.split(" ").length - 1);
+const firstLetter = name[0]
+const lastLetter = name[name.length -1]
 console.log(`Hello ${name}.
-Your name is ${totalLetters} letters long
+Your name is ${(totalLetters - spaceCount)} letters long
 The first letter of your name is ${firstLetter}, the last is ${lastLetter}.
 If I wanted to be funny, I would call you Winschmoo`)
